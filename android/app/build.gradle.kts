e9c89343d78fbe67247c83783352a8081f8e5177
ScreenTinker <hello@screentinker.com>
2026-06-10 14:12:47 -0500

    chore(release): v1.8.1

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.remotedisplay.player"
         minSdk = 26
         targetSdk = 34
-        versionCode = 16
-        versionName = "1.8.0"
+        versionCode = 17
+        versionName = "1.8.1"
     }
 
     signingConfigs {
