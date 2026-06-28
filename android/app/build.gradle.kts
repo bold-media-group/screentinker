d9fb914b9edaeef102744fd8981e04d125173dfa
ScreenTinker <hello@screentinker.com>
2026-06-27 19:59:34 -0500

    chore(release): v1.9.2-beta1

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.remotedisplay.player"
         minSdk = 24
         targetSdk = 34
-        versionCode = 30
-        versionName = "1.9.1"
+        versionCode = 31
+        versionName = "1.9.2-beta1"
     }
 
     signingConfigs {
